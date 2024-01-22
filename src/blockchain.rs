cd1ec8ed77d0818335b288d92fca0922fdff6bc9
itsmepravin <singh.pravin19@yahoo.com>
2024-01-22 21:24:21 +0545

    blockchain structs added

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -22,3 +22,18 @@ pub struct Blockheader {
     merkle: String,
     difficulty: u32,
 }
+
+#[derive(Serialize, Debug)]
+pub struct Block {
+    header: Blockheader,
+    count: u32,
+    transactions: Vec<Transaction>,
+}
+
+pub struct Chain {
+    chain: Vec<Block>,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String,
+    reward: f32,
+}

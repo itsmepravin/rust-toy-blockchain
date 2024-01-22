0184f57fb3a33244a80bcd5feacfd5b4d2d9a1c1
itsmepravin <singh.pravin19@yahoo.com>
2024-01-22 21:48:08 +0545

    Block add function

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -37,3 +37,100 @@ pub struct Chain {
     miner_addr: String,
     reward: f32,
 }
+
+impl Chain {
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
+        let mut chain = Chain {
+            chain: Vec::new(),
+            curr_trans: Vec::new(),
+            difficulty,
+            miner_addr,
+            reward: 100.0,
+        };
+
+        chain.generate_new_block();
+        chain
+    }
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction {
+            sender,
+            receiver,
+            amount,
+        });
+
+        true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap(),
+        };
+        Chain::hash(&block.header)
+    }
+
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
+
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        true
+    }
+
+    pub fn generate_new_block(&mut self) {
+        let header = Blockheader {
+            timestamp: time::now().to_timespec().sec,
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            difficulty: self.difficulty,
+        };
+
+        let reward_trans = Transaction {
+            sender: String::from("Root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward,
+        };
+
+        let mut block = Block {
+            header,
+            count: 0,
+            transactions: vec![],
+        };
+
+        block.transactions.push(reward_trans);
+        block.transactions.append(&mut self.curr_trans);
+        block.count = block.transactions.len() as u32;
+        block.header.merkle = Chain::get_merkle(block.transactions.clone());
+        Chain::proof_of_work(&mut block.header);
+
+        println!("{:?}", &block);
+        self.chain.push(block);
+        true
+    }
+
+    fn get_merkle(curr_trans: Vec<Transaction>) -> String {
+        let mut merkle = Vec::new();
+
+        for t in &curr_trans {
+            let hash = Chain::hash(t);
+            merkle.push(hash);
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().cloned().unwrap();
+            merkle.push(last);
+        }
+
+        while merkle.len() > 1 {
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Chain::hash(&h1);
+            merkle.push(nh);
+        }
+        merkle.pop().unwrap()
+    }
+}
